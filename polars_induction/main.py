031a27c6035d3ed673c67f12649db349fcd96069
federico.lavatori@gmail.com <federico.lavatori@gmail.com>
2023-03-26 22:11:52 +0200

    polars sql and basic r/w ops

--- polars_induction/main.py
+++ polars_induction/main.py
@@ -11,3 +11,23 @@ series = pl.Series("a", [1, 2, 3, 4, 5])
 print(series)
 
 
+dataframe = pl.DataFrame({"integer": [1, 2, 3],
+                          "date": [
+                              (datetime(2022, 1, 1)),
+                              (datetime(2022, 1, 2)),
+                              (datetime(2022, 1, 3))
+                          ],
+                          "float":[4.0, 5.0, 6.0]})
+
+print(dataframe)
+
+dataframe.write_csv('output.csv')
+
+df_csv_with_dates = pl.read_csv('output.csv', try_parse_dates=True)
+
+print(df_csv_with_dates)
+
+
+
+
+
